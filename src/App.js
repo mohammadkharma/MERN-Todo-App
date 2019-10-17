12ac12b8621bc26215b532762468c5479abf9666
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-17 23:42:52 +0200

    create components folder with 3 components inside it: todo-list / create-todo / edit-todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CreateTodo from "./components/create-todo.component";
+import TodosList from "./components/todos-list.component";
+import EditTodo from "./components/edit-todo.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <h1>MERN</h1>
+        </div>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </Router>
+    );
+  }
 }
 
 export default App;
