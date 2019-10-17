7710270bac58ba82c775ecf4b1f9132b2784f8a7
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-17 22:14:22 +0200

    Updates

---
+++ precache-manifest.20619ec24690a55ef28699bc84799b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f8602314f2bdaeaa705453bc745510",
+    "url": "/MERN-Todo-App/index.html"
+  },
+  {
+    "revision": "d107ddde1d5bfcf46e51",
+    "url": "/MERN-Todo-App/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "75c906ac25941554ed1b",
+    "url": "/MERN-Todo-App/static/js/2.fa1e3327.chunk.js"
+  },
+  {
+    "revision": "d107ddde1d5bfcf46e51",
+    "url": "/MERN-Todo-App/static/js/main.7efda64f.chunk.js"
+  },
+  {
+    "revision": "4e184de5a7442ce5e278",
+    "url": "/MERN-Todo-App/static/js/runtime-main.9377b9c0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MERN-Todo-App/static/media/logo.25bf045c.svg"
+  }
+]);
