5379c9f198e4494c291a5c8ee9cad73fd654a2b9
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-21 12:09:07 +0200

    added initial navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CreateTodo from "./components/create-todo.component";
 import TodosList from "./components/todos-list.component";
 import EditTodo from "./components/edit-todo.component";
 
 class App extends Component {
+
   render() {
     return (
       <Router>
         <div className="container">
-          <h1>MERN</h1>
+
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a href="/" className="navbar-brand"><h1>TODOS</h1></a>
+            {/* <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link> */}
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="navbar-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="navbar-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
       </Router>
     );
   }
+
 }
 
 export default App;
