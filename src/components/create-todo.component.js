5c99de198e1808acf5daa35d7bad2c02533a1d9d
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-27 13:55:42 +0100

    add state with 4 default properties

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 export default class CreateTodo extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+    
     render() {
         return (
             <div>
