5400c07c9e2d308323073bb749e178a254afc529
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-27 14:20:06 +0100

    add methods

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -5,6 +5,11 @@ export default class CreateTodo extends Component {
     constructor(props){
         super(props);
 
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
         this.state = {
             todo_description: '',
             todo_responsible: '',
@@ -12,7 +17,44 @@ export default class CreateTodo extends Component {
             todo_completed: false
         }
     }
+
+    onChangeTodoDescription(e){
+        this.setState({
+            todo_description: e.target.value
+        });
+    }
+
+    onChangeTodoResponsible(e){
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+
+    onChangeTodoPriority(e){
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        console.log("Form submitted");
+        console.log(`Form Description: ${this.state.todo_description}`);
+        console.log(`Form Responsible: ${this.state.todo_responsible}`);
+        console.log(`Form Priority: ${this.state.todo_priority}`);
+        console.log(`Form Completed: ${this.state.todo_completed}`);
+
+        this.setState({
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        })
+    }
+
     
+
     render() {
         return (
             <div>
